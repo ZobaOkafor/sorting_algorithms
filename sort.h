de97b99b9d42d8cc1098a4df104d605637c013a3
ZobaOkafor <judithokafor30@gmail.com>
2024-01-20 06:26:15 -0800

    Header file

--- sort.h
+++ sort.h
@@ -11,6 +11,23 @@
 #define DOWN 1
 
 
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
+
 /* Helper function prototypes for printing */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
