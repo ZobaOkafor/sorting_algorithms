43c1aa6a198006b1e10ff51c8b459c7a81ca5336
ZobaOkafor <judithokafor30@gmail.com>
2024-01-20 06:42:54 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,7 +20,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 
