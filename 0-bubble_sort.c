735ecdae341905ba874454d440648b31bb88a84e
ZobaOkafor <judithokafor30@gmail.com>
2024-01-22 14:27:49 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int swapped;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
@@ -34,4 +37,19 @@ void bubble_sort(int *array, size_t size)
 		if (swapped == 0)
 			break;
 	}
+}
+
+
+
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
 }
