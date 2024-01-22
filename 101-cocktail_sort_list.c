d029ab7037cb080dffdfecdcbebd2b8d08066dd6
ZobaOkafor <judithokafor30@gmail.com>
2024-01-22 13:50:05 -0800

    Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,6 +2,40 @@
 
 
 /**
+ * swap - swaps the elements of the list
+ * @ptr1: first pointer
+ * @ptr2: second pointer
+ * @n: n is 0 for increase, n is 1 for decrease
+ *
+ */
+
+void swap(listint_t **ptr1, listint_t **ptr2, int n)
+{
+	listint_t *aux, *tmp;
+
+	aux = *ptr1;
+	tmp = *ptr2;
+
+	aux->next = tmp->next;
+	tmp->prev = aux->prev;
+
+	if (tmp->next)
+		tmp->next->prev = aux;
+
+	if (aux->prev)
+		aux->prev->next = tmp;
+
+	aux->prev = tmp;
+	tmp->next = aux;
+
+	if (n == 0)
+		*ptr1 = tmp;
+	else
+		*ptr2 = aux;
+}
+
+
+/**
  * liststart_sort - move the bigger numbers to the end
  * @ptr: pointer to the bigger number
  * @start: end of the list
